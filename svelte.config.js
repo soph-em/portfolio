e74380dbe614669dfb2429b6d0a94abf17c32e0f
soph-em <sophie.earl@live.com>
2023-07-15 22:07:38 +0100

    relative images test commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import {mdsvex} from 'mdsvex';
-import relativeImages from "mdsvex-relative-images";
+// import relativeImages from "mdsvex-relative-images";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
     preprocess: [
         mdsvex({
             extensions: ['.md', '.svx'],
-			remarkPlugins: [relativeImages],
+			// remarkPlugins: [relativeImages],
         }), vitePreprocess()
     ],
 
